ee04d1c718c06b334c0788fa7fd8955658a23302
itupitsyn <itupitsyn@ptsecurity.com>
2024-09-29 04:35:35 +0300

    fix:(example): new toasts

--- template/my_component/example.py
+++ template/my_component/example.py
@@ -1,13 +1,14 @@
 import streamlit as st
 import os
 import streamlit.components.v1 as components
+import random
 
 parent_dir = os.path.dirname(os.path.abspath(__file__))
 build_dir = os.path.join(parent_dir, "frontend/build")
-_component_func = components.declare_component("my_component", path=build_dir)
+_component_func = components.declare_component("extremely_serious_toaster", path=build_dir)
 
-def my_component(name, key=None):
-    component_value = _component_func(name=name, key=key, default=0)
+def extremely_serious_toaster(name, maxWidth=150, time=0, key=None):
+    component_value = _component_func(name=name, maxWidth=maxWidth, time=time, key=key)
 
     # We could modify the value returned from the component if we wanted.
     # There's no need to do this in our simple example - but it's an option.
@@ -23,7 +24,7 @@ st.subheader("Component with constant args")
 # Create an instance of our component with a constant `name` arg, and
 # print its output value.
 if st.button('click me', 123):
-    my_component("<a href='https://www.yandex.ru'>Здравствуйте, а что здеся</a>")
+    extremely_serious_toaster("<a href='https://www.yandex.ru'>Здравствуйте, а что здеся</a>", 200, 0, random.randint(0, 1000))
 
 if st.button('click me', 456):
-    my_component("Курс FX Spot по валютной паре EUR/RUB вырос на 4.05% по итогам последнего дня. <a href='https://www.google.com'>По направлению куда следует!</a>")
+    extremely_serious_toaster("Курс FX Spot по валютной паре EUR/RUB вырос на 4.05% по итогам последнего дня. <a href='https://www.google.com'>По направлению куда следует!</a>", 100, 3000, random.randint(0, 1000))
